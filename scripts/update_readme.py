f964abc8b03e0733a2b74597dfda4b897975d8c1
David Marx <david.marx84@gmail.com>
2022-08-15 21:37:27 -0700

    Update update_readme.py

--- scripts/update_readme.py
+++ scripts/update_readme.py
@@ -53,7 +53,7 @@ TOC = sorted(TOC, key=lambda x:x['last_modified'])[::-1]
 url_root = '' # "https://github.com/dmarx/bench-warmers/blob/main/"
 
 header= "|last_modified|title|est. idea maturity|tags\n|:---|:---|---:|:---|\n"
-recs = [f"|{d['last_modified']}|[{d['title']}]({url_root}{d['fpath']})|{d['n_char']}|{make_badges(d['n_char'])}|" for d in TOC]
+recs = [f"|{d['last_modified']}|[{d['title']}]({url_root}{d['fpath']})|{d['n_char']}|{make_badges(d['tags'])}|" for d in TOC]
 toc_str= header + '\n'.join(recs)
 
 #readme_stub = "# title \n\n text goes here\n\n{TOC}\n\n# another section"
