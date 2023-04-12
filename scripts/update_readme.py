a03b2d604df2b957d6428b2272defe08edff0bcf
David Marx <david.marx84@gmail.com>
2023-04-11 17:11:12 -0700

    Update update_readme.py

--- scripts/update_readme.py
+++ scripts/update_readme.py
@@ -12,11 +12,8 @@ def get_last_modified_date(fpath, verbose=True, timestamp=False):
     if timestamp:
         fmt="%at"
     # ignore commits where author=action@github.com
-    #cmd = f"git log --author='^(?!action).*$' --perl-regexp --pretty=format:{fmt} --".split( )
-    
-    # only consider commits by specific author  
-    #cmd = f"git log --author=dmarx --perl-regexp --pretty=format:{fmt} --".split() # need to adjust this for local user
-    cmd = f"git log --pretty=format:{fmt} --".split() # straight killin me here...
+    cmd = f"git log --author='^(?!.*action).*$' --perl-regexp --pretty=format:{fmt} --".split( )
+    #cmd = f"git log --pretty=format:{fmt} --".split() # straight killin me here...
     cmd += [str(fpath)]
     if verbose:
         logger.debug(cmd)
